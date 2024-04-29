0baf424d623278ce49296c56d8d20a577879691c
KhuongVo <21130407@st.hcmuaf.edu.vn>
2024-04-29 16:28:39 +0700

    update GPA to 8.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+buildscript {
+    val agp_version by extra("8.3.2")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.1" apply false
+    id("com.android.application") version "8.3.2" apply false
 //    Theo cấu trúc file ban đầu của Khải Nam thì mình sẽ sử dụng 8.3.1 nha.
 //    id("com.android.application") version "8.1.2" apply false
 }
