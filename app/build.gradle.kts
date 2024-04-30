d5b316953eb06c575052e75e34d1360704860b65
KhuongVo <21130407@st.hcmuaf.edu.vn>
2024-04-30 11:04:39 +0700

    xoá thư mục placeholder bị trùng, thêm 2 thư viện vào trong build.gralde

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+//    implement loading library
+    implementation ("com.squareup.picasso:picasso:2.71828")
+//    thư viện thêm - 21130463
+    implementation("androidx.cardview:cardview:1.0.0")
 }
