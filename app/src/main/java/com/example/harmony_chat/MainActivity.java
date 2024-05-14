53e22b84a25cbb7051df053c46c6c59241c0d28e
KhuongVo <21130407@st.hcmuaf.edu.vn>
2024-05-14 20:00:03 +0700

    thêm lại sự kiện tìm kiếm cho ứng dụng

--- app/src/main/java/com/example/harmony_chat/MainActivity.java
+++ app/src/main/java/com/example/harmony_chat/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.harmony_chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.makeramen.roundedimageview.RoundedImageView;
@@ -15,8 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        hideSystemUI();
+
         final RoundedImageView userAvatar = findViewById(R.id.user_avatar);
         final LinearLayout avatarMenu = findViewById(R.id.avatar_menu);
+        boolean isSearchVisible = false;
 
         userAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +46,39 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        // Hiển thị hoặc ẩn button tìm kiếm tùy thuộc vào trạng thái của thanh tìm kiếm
+        ImageView find = findViewById(R.id.search_icon);
+        find.setVisibility(isSearchVisible ? View.GONE : View.VISIBLE);
+
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoSearchUser();
+            }
+        });
+    }
+
+    public void gotoSearchUser(){
+        Intent intent = new Intent(this, SearchUserActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoMyProfile() {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+    }
+
+    private void hideSystemUI() {
+        // Ẩn thanh trạng thái và thanh điều hướng
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
     }
 }
