7505a72a9ca56eb86fed26ef75f81f02e2767f7b
KhuongVo <21130407@st.hcmuaf.edu.vn>
2024-05-14 20:36:12 +0700

    thực hiện import lại các icon theo dạng xml, chỉnh sửa lại fragment của fragment_profile_settings.xml, chuẩn bị thực hiện thay đổi giao diện trong MainActivity.xml

--- app/src/main/java/com/example/harmony_chat/ProfileFragment.java
+++ app/src/main/java/com/example/harmony_chat/ProfileFragment.java
@@ -13,10 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
-
-    LinearLayout settingBtn, profileBtn;
     TextView setting, profile;
-
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -27,36 +24,19 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile_settings, container, false);
-
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        settingBtn = view.findViewById(R.id.settingBtn);
-        profileBtn = view.findViewById(R.id.profileBtn);
         setting = view.findViewById(R.id.text_setting);
         profile = view.findViewById(R.id.text_profile);
-
-        settingBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                gotoSetting();
-            }
-        });
 
         setting.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 gotoSetting();
-            }
-        });
-
-        profileBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                gotoMyProfile();
             }
         });
 
