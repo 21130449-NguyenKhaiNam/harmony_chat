04d91cfb1ebd7202484f995f5006a1a5b14f5186
KhuongVo2105 <21130407@st.hcmuaf.edu.vn>
2024-05-24 01:39:41 +0700

    cập nhật chức năng đổ dữ liệu vào trong RecycleView, chuyển màn hình tới chatscreen, chưa thực hiện chức năng gì liên quan tới ChatScreen cả

---
+++ app/src/main/java/com/example/harmony_chat/Item/ChatItem.java
@@ -0,0 +1,31 @@
+package com.example.harmony_chat.Item;
+
+public class ChatItem {
+    private String title;
+    private String message;
+    private String avatarUrl;
+    private String time;
+
+    public ChatItem(String title, String message, String avatarUrl, String time) {
+        this.title = title;
+        this.message = message;
+        this.avatarUrl = avatarUrl;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
--- app/src/main/java/com/example/harmony_chat/MainActivity.java
+++ app/src/main/java/com/example/harmony_chat/MainActivity.java
@@ -3,27 +3,44 @@ package com.example.harmony_chat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import com.example.harmony_chat.Adapter.ChatAdapter;
+import com.example.harmony_chat.Adapter.SelectListener;
+import com.example.harmony_chat.Item.ChatItem;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.makeramen.roundedimageview.RoundedImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SelectListener {
 
     private CardView avatar;
     private ImageView find;
 
     private TextView setting, profile;
+    private RecyclerView chatRecyclerView;
+    private ChatAdapter chatAdapter;
+    private List<ChatItem> chatItemList;
     private com.example.harmony_chat.model.Profile myProfile;
 
     @Override
@@ -53,6 +70,39 @@ public class MainActivity extends AppCompatActivity {
                 gotoSearchUser();
             }
         });
+
+        // Initialize RecyclerView
+        chatRecyclerView = findViewById(R.id.chat_recycler_view);
+        chatRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        chatItemList = new ArrayList<>();
+        String urlImage = "https://images.unsplash.com/photo-1627087820883-7a102b79179a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+        FirebaseFirestore.getInstance()
+                .collection("PROFILES")
+                .get()
+                .addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        for (DocumentSnapshot document : task.getResult()) {
+//                            com.example.harmony_chat.model.Profile pro5 = document.toObject(com.example.harmony_chat.model.Profile.class);
+//                            LocalDate localDate = LocalDate.now();
+//                            chatItemList.add(new ChatItem(pro5.getUsername(), "message", urlImage, localDate.toString()));
+//                        }
+//                    }
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+//                            Log.d(TAG, document.getId() + " => " + document.getData());
+                            com.example.harmony_chat.model.Profile pro5 = document.toObject(com.example.harmony_chat.model.Profile.class);
+                            LocalDate localDate = LocalDate.now();
+                            chatItemList.add(new ChatItem(pro5.getUsername(), "message", urlImage, localDate.toString()));
+                        }
+                        // Sau khi đã lấy được dữ liệu từ Firestore và thêm vào chatItemList
+                        // Bây giờ ta tạo adapter và thiết lập cho RecyclerView
+                        chatAdapter = new ChatAdapter(chatItemList, this);
+                        chatRecyclerView.setAdapter(chatAdapter);
+                        chatAdapter.notifyDataSetChanged();
+                    } else {
+                        Log.e("MAIN_ACTIVITY ERROR", "Error getting documents: ", task.getException());
+                    }
+                });
     }
 
     private void createPopUpWindow() {
@@ -115,5 +165,11 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         );
+    }
+
+    @Override
+    public void onItemClicked(ChatItem chatItem) {
+        Intent intent = new Intent(this, ChatScreen.class);
+        startActivity(intent);
     }
 }
